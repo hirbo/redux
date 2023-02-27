1feabb0b7eeead8de0c9bd142616fe42c512b24f
hirbo <hirbobalasa@gmail.com>
2023-02-28 01:19:02 +0300

    basic redux tutorial

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-
+import Counter from "./features/counter/Counter";
 
 function App() {
+  
+
   return (
     <div className="App">
-       React Redux
+      
+      <h1>
+        REACT REDUX
+        <Counter/>
+      </h1>
     </div>
   );
 }
+
 
 export default App;
---
+++ src/features/counter/Counter.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { useSelector,useDispatch } from 'react-redux'
+import { increment,decrement,reset,incrementbyAmount } from './counterSlice'
+import { useState } from 'react';
+
+
+function Counter() {
+
+const count = useSelector((state) => state.counter.count);
+const dispatch = useDispatch();
+const [amount,setAmount] =  useState(0);
+const addValue = Number(amount) || 0
+  return (
+    <div>
+        <p>
+            {count}
+        </p>
+        <button onClick={() =>{dispatch(increment())}}>
+                +1
+        </button>
+        <button onClick={() =>{dispatch(decrement())}}>
+                -1
+        </button>
+        <button onClick={() =>{dispatch(reset())}}>
+                reset
+        </button>
+        <input
+        value={amount}
+        onChange={(e)=>setAmount(e.target.value)}
+        type='number'
+        >
+        </input>
+        <button onClick={() =>{dispatch(incrementbyAmount(addValue))}}>
+                inc by amount
+        </button>
+        
+    </div>
+  )
+}
+
+export default Counter
